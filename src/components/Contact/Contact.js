e5313d47a6784d703b0b131f35a8436bcdf7daeb
mickael keita <keitamickael@gmail.com>
2023-04-14 16:20:39 +0200

    add some space in contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,22 +8,22 @@ export default function Conatct() {
       className="bg-darkSecondary text-darkText text-lg h-screen flex items-center justify-center"
     >
       <div className="px-2 max-w-7xl mx-auto">
-        <div className="w-full flex flex-col lg:flex-row lg:items-start lg:justify-center gap-5">
-          <div className="text-left mb-10 lg:max-w-[300px]">
+        <div className="w-ful flex flex-col lg:flex-row lg:items-start lg:justify-center gap-5">
+          <div className="text-left px-4 mb-10 w-11/12 lg:max-w-[300px]">
             <h2 className="text-darkHeading text-3xl uppercase mb-5 lg:mb-20">
               Contact
             </h2>
-            <p className="ml-4 mb-4 lg:ml-0 lg:mb-10">
+            <p className="mb-4 lg:mb-10">
               N&apos;hésitez pas à me contacter, je vous répondrai le plus tôt
               possible.
             </p>
-            <p className="mb-4 ml-4 flex gap-3 lg:ml-0">
+            <p className="mb-4 flex gap-3 lg:ml-0">
               <BsFillTelephoneFill /> 06 23 76 91 51
             </p>
-            <p className="mb-4 ml-4 flex gap-3 lg:ml-0 lg:mb-20">
+            <p className="mb-4 flex gap-3 lg:ml-0 lg:mb-20">
               <AiFillMail /> keitamickaelpro@gmail.com
             </p>
-            <ul className="ml-4 mt-10 text-2xl text-darkHeading flex items-center justify-center lg:justify-evenly gap-5">
+            <ul className="mt-10 text-2xl text-darkHeading flex items-center justify-center lg:justify-evenly gap-5">
               <li className="cursor-pointer hover:scale-110">
                 <BsGithub />
               </li>
@@ -33,7 +33,7 @@ export default function Conatct() {
             </ul>
           </div>
           <div className="flex items-center justify-center w-full lg:max-w-[400px]">
-            <form className="w-full max-w-[400px] bg-darkTertiary rounded-md p-5 flex flex-col gap-8">
+            <form className="w-11/12 max-w-[400px] bg-darkTertiary rounded-md p-5 flex flex-col gap-8">
               <div className="flex flex-col gap-2">
                 <label htmlFor="name">Votre Nom</label>
                 <input
