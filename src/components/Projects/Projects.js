0b370c224a8ba94487ba01dec6b161a157b86dfa
mickael keita <keitamickael@gmail.com>
2023-04-14 15:41:17 +0200

    style: adjustement of project section style

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import ProjectsList from "@/components/Projects/components/ProjectsList";
 
 export default function Projects() {
   return (
-    <section className="bg-darkBg ">
+    <section className="bg-darkBg min-h-screen">
       <div className="px-2 max-w-7xl mx-auto">
         <h2 className="text-darkHeading text-4xl uppercase pt-20 pb-10">
           Projets
