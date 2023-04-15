c4e71b2169e969fcb6eca02c160a772a13055948
mickael keita <keitamickael@gmail.com>
2023-04-15 16:17:16 +0200

    style: change contact section layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -79,18 +79,18 @@ export default function Conatct() {
   return (
     <section
       id="contact"
-      className="bg-darkSecondary text-darkText text-lg h-screen flex items-center justify-center"
+      className="bg-darkSecondary text-darkText text-lg h-screen flex flex-col items-center justify-center lg:justify-around"
     >
+      {messageHasBeenSent && (
+        <div className="flex items-center justify-center w-full">
+          <div className="flex items-center gap-5 bg-darkSuccess text-darkSecondary w-11/12 max-w-xs pl-2 py-2 rounded-md">
+            <AiFillCheckCircle /> Votre message a bien été envoyé.
+          </div>
+        </div>
+      )}
       <div className="px-2 max-w-7xl mx-auto">
         <div className="w-ful flex flex-col lg:flex-row lg:items-start lg:justify-center gap-5">
-          {messageHasBeenSent && (
-            <div className="flex items-center justify-center">
-              <div className="flex items-center gap-5 bg-darkSuccess text-darkSecondary w-11/12 max-w-xs pl-2 py-2 rounded-md">
-                <AiFillCheckCircle /> Votre message a bien été envoyé.
-              </div>
-            </div>
-          )}
-          <div className="text-left px-4 mb-10 w-11/12 lg:max-w-[300px]">
+          <div className="text-left px-4 mb-10 w-11/12 lg:max-w-[600px]">
             <h2 className="text-darkHeading text-3xl uppercase mb-5 lg:mb-20">
               Contact
             </h2>
@@ -120,9 +120,9 @@ export default function Conatct() {
               </li>
             </ul>
           </div>
-          <div className="flex items-center justify-center w-full lg:max-w-[400px]">
+          <div className="flex items-center justify-center w-full lg:max-w-[600px]">
             <form
-              className="w-11/12 max-w-[400px] bg-darkTertiary rounded-md p-5 flex flex-col gap-8"
+              className="w-11/12 max-w-[600px] bg-darkTertiary rounded-md p-5 flex flex-col gap-8"
               onSubmit={handleSubmit(sendMessage)}
             >
               <div className="flex flex-col gap-2">
