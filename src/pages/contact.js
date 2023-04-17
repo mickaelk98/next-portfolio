fc0d141f32fa92b26cc42235ae30072354a2afb8
mickael keita <keitamickael@gmail.com>
2023-04-17 20:26:20 +0200

    add metadata for each page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
@@ -87,6 +88,15 @@ export default function Conatct() {
 
   return (
     <>
+      <Head>
+        <title>Mickael Keita - Portfolio | Contact</title>
+        <meta
+          name="description"
+          content="Bienvenue sur mon portfolio, je suis mickael keita jeune développeur web formé chez openclassroom sur ce site vous verrez différents projets que j'ai réalisé et vous pourrez me contacter directement, alors n'hésitez pas."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/logo.png" />
+      </Head>
       <TransitionEffect />
       <Header />
       <section
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "@/components/Header/Header";
 import ProjectsList from "@/components/Projects/components/ProjectsList";
 import TransitionEffect from "@/components/TransitionEffect/TransitionEffect";
@@ -5,6 +6,15 @@ import TransitionEffect from "@/components/TransitionEffect/TransitionEffect";
 export default function Projects() {
   return (
     <>
+      <Head>
+        <title>Mickael Keita - Portfolio | Projets</title>
+        <meta
+          name="description"
+          content="Bienvenue sur mon portfolio, je suis mickael keita jeune développeur web formé chez openclassroom sur ce site vous verrez différents projets que j'ai réalisé et vous pourrez me contacter directement, alors n'hésitez pas."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/logo.png" />
+      </Head>
       <TransitionEffect />
       <Header />
       <section
