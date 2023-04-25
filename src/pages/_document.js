911a2be1e7f290ed5211d834eeaf725763f5a07f
mickael keita <keitamickael@gmail.com>
2023-04-25 12:28:06 +0200

    change lang in fr

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="scroll-smooth" lang="en">
+    <Html lang="fr">
       <Head />
       <body>
         <Main />
